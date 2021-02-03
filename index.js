43165db9cb9e525961c07e579828bcbc97e5de7d
Alfred <ya95123@gmail.com>
2021-02-03 17:33:30 +0800

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleOpen = document.getElementsByClassName("toggleOpen")
+
+for (let i = 0; i < toggleOpen.length; i++) {
+  // toggleOpen[i].onclick = () => {
+  //   alert("click!")
+  // }
+}
+
+toggleOpen[0].click = () => {
+  alert("click!")
+}
